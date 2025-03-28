e2eae0d8b3dfae98bca7ac15af76845698cb1f62
Chawanwit Akarajirathanachot <76256502+Karibura-Cyber@users.noreply.github.com>
2025-03-29 01:47:03 +0700

    [ADD] Initial commit

---
+++ extension.js
@@ -0,0 +1,20 @@
+({
+    name: "MPU6050", // Category Name
+    description: "6-Axis (Gyro + Accelerometer) MEMS MotionTracking Devices with I2C Interface",
+    author: "NightHawk Robotics",
+    category: "Sensors",
+    version: "0.0.1",
+    icon: "/static/icon.png", // Category icon
+    color: "#E74C3C", // Category color (recommend some blocks color)
+    blocks: [ // Blocks in Category
+        "mpu6050_init",
+        "mpu6050_update",
+        "mpu6050_get_acc",
+        "mpu6050_get_gyro",
+        "mpu6050_get_angle"
+    ],
+    supportArduinoPlatform: true,
+    depends: [ // Arduino library
+        "MPU6050_light@1.2.1"
+    ]
+});
